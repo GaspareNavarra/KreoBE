0590d97d9e0bed7db3ad2dbce4307132f2c67ddd
Gaspare Navarra <gaspare.navarra@labitech.com>
2024-07-18 17:58:50 +0200

    Added token in call

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'https://kreo-liard.vercel.app'); //'http://localhost:5173','https://kreo-liard.vercel.app'
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'x-Requested-With,content-type,datainizio,datafine,token,schema');
+  res.setHeader('Access-Control-Allow-Headers', 'x-Requested-With,content-type,datainizio,datafine,token,schema,Authorization');
   next();
 });
 
@@ -25,40 +25,50 @@ app.get('/', async (req, res, next) => {
 
 app.post('/send-mail', async (req, res) => {
   
-  console.log(res);
-  console.log(req);
-
+  const {authorization} = req.headers;
   const {email} = req.body;
   const {subject} = req.body;
   const {text} = req.body;
-
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: 'gaspare.navarra4@gmail.com',
-      pass: 'mzdr ifot mevj odyn',
-    },
-  });
-
-  const msg = {
-    from: 'Kreo <kreo@gmail.com>',
-    to: email,
-    subject: subject,
-    text: text,
+  const config = {
+    headers: { 'Authorization': authorization }
   };
 
-  const info = await transporter.sendMail(msg);
-
-  console.log('Message sent: %s', info.messageId);
-
-  res.status(200).send(msg);
+  axios.get('https://x8ki-letl-twmt.n7.xano.io/api:Fh-KZon-/auth/me', config)
+  .then(async (response) => {
+    if(response.status!=200) res.status(response.status).send({ data: response.data});
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'gaspare.navarra4@gmail.com',
+        pass: 'mzdr ifot mevj odyn',
+      },
+    });
+  
+    const msg = {
+      from: 'Kreo <kreo@gmail.com>',
+      to: email,
+      subject: subject,
+      text: text,
+    };
+  
+    await transporter.sendMail(msg);
+  
+    res.status(200).send(msg);
+  }).catch((error) => {
+    console.log(error);
+  });
 });
 
 app.get('/search-customer', async (req, res) => {
+  const {authorization} = req.headers;
+  const config = {
+    headers: { 'Authorization': authorization }
+  };
 
-  axios.get('https://x8ki-letl-twmt.n7.xano.io/api:Fh-KZon-/customer').then((response) => {
+  axios.get('https://x8ki-letl-twmt.n7.xano.io/api:Fh-KZon-/customer', config)
+  .then((response) => {
     console.log(response);
     let data = response.data;
 
