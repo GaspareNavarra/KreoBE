0d0a050ba6b06d00b7676b7250c41ffbc0e8d568
Gaspare Navarra <gaspare.navarra@labitech.com>
2025-03-16 23:25:04 +0100

    Added fix

--- app.js
+++ app.js
@@ -1,81 +1,112 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-const { default: axios } = require('axios');
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const { default: axios } = require("axios");
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://kreo-liard.vercel.app'); //'http://localhost:5173','https://kreo-liard.vercel.app'
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'x-Requested-With,content-type,datainizio,datafine,token,schema,Authorization');
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173"); //'http://localhost:5173','https://kreo-liard.vercel.app'
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "x-Requested-With,content-type,datainizio,datafine,token,schema,Authorization"
+  );
   next();
 });
 
-app.get('/', async (req, res, next) => {
-  console.log('Working...');
-  res.send('This is the main page!');
+app.get("/", async (req, res, next) => {
+  console.log("Working...");
+  res.send("This is the main page!");
   // next();
 });
 
-app.post('/send-mail', async (req, res) => {
-  
-  const {authorization} = req.headers;
-  const {email} = req.body;
-  const {subject} = req.body;
-  const {text} = req.body;
+app.post("/send-mail", async (req, res) => {
+  const { authorization } = req.headers;
+  const { email } = req.body;
+  const { subject } = req.body;
+  const { text } = req.body;
   const config = {
-    headers: { 'Authorization': authorization }
+    headers: { Authorization: authorization },
   };
 
-  axios.get('https://x8ki-letl-twmt.n7.xano.io/api:Fh-KZon-/auth/me', config)
-  .then(async (response) => {
-    if(response.status!=200) res.status(response.status).send({ data: response.data});
-    let transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
-      auth: {
-        user: 'gaspare.navarra4@gmail.com',
-        pass: 'mzdr ifot mevj odyn',
-      },
+  axios
+    .get("https://x8ki-letl-twmt.n7.xano.io/api:Fh-KZon-/auth/me", config)
+    .then(async (response) => {
+      if (response.status != 200)
+        res.status(response.status).send({ data: response.data });
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: "gaspare.navarra4@gmail.com",
+          pass: "mzdr ifot mevj odyn",
+        },
+      });
+
+      const msg = {
+        from: "Kreo <kreo@gmail.com>",
+        to: email,
+        subject: subject,
+        text: text,
+      };
+
+      await transporter.sendMail(msg);
+
+      res.status(200).send(msg);
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  
-    const msg = {
-      from: 'Kreo <kreo@gmail.com>',
-      to: email,
-      subject: subject,
-      text: text,
-    };
-  
-    await transporter.sendMail(msg);
-  
-    res.status(200).send(msg);
-  }).catch((error) => {
-    console.log(error);
-  });
 });
 
-app.get('/search-customer', async (req, res) => {
-  const {authorization} = req.headers;
+app.get("/search-customer", async (req, res) => {
+  const { authorization } = req.headers;
   const config = {
-    headers: { 'Authorization': authorization }
+    headers: { Authorization: authorization },
   };
 
-  axios.get('https://x8ki-letl-twmt.n7.xano.io/api:Fh-KZon-/customer', config)
-  .then((response) => {
-    console.log(response);
-    let data = response.data;
+  axios
+    .get("https://x8ki-letl-twmt.n7.xano.io/api:Fh-KZon-/customer", config)
+    .then((response) => {
+      console.log(response);
+      let data = response.data;
 
-    return res.status(200).send(data);
-  }).catch((error) => {
-    console.log(error);
-    return res.status(404).send({result: 404});
-  });
+      return res.status(200).send(data);
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(404).send({ error: error });
+    });
 });
-app.listen(port, () => console.log('App di esempio per http://localhost:' + port));
+
+app.post("/save-entrance", async (req, res) => {
+  const { authorization } = req.headers;
+  const { body } = req;
+  const config = {
+    headers: { Authorization: authorization },
+  };
+
+  axios
+    .post(
+      "https://x8ki-letl-twmt.n7.xano.io/api:Fh-KZon-/ingressi",
+      body,
+      config
+    )
+    .then((response) => {
+      //TODO: Aggiungere logica per cui se ci sono troppi ingressi per il customer selezionato allora si cancella
+      // l'ultimo record e si memorizza l'ultimo ingresso fatto
+    });
+});
+app.listen(port, () =>
+  console.log("App di esempio per http://localhost:" + port)
+);
